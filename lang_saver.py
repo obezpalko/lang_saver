c8d70904425e800f192260e7bcf139c06387565a
Alex Bezpalko <alexbe@wix.com>
2018-12-04 16:16:32 +0200

    rewrite to module and add logging

--- lang_saver.py
+++ lang_saver.py
@@ -3,32 +3,56 @@
 """ save language per window
 """
 
+import logging
+from logging.handlers import SysLogHandler
 from time import sleep
 from xkbgroup import XKeyboard
 from Xlib.display import Display
+import setproctitle
+import sys
 
-WINDOW_HISTORY = {}
-DISPLAY = Display()
-XKB = XKeyboard()
-PREV_WINDOW = 0
-PREV_GROUP = XKB.group_num
 
-while True:
-    WINDOW = DISPLAY.get_input_focus().focus.id
-    if WINDOW != PREV_WINDOW:
-        #  window has been changed
-        #  need to check if current group match stored
-        #  and if not - restore from stored
-        if WINDOW in WINDOW_HISTORY:
-            XKB.group_num = WINDOW_HISTORY[WINDOW]
-        else:
-            WINDOW_HISTORY[WINDOW] = XKB.group_num
-        PREV_WINDOW = WINDOW
-    else:
-        # window wans't changed
-        # probably group changed
-        # check previous group and if not match - update history
-        if PREV_GROUP != XKB.group_num:
-            WINDOW_HISTORY[WINDOW] = XKB.group_num
-            PREV_GROUP = XKB.group_num
-    sleep(0.25)
+class LangSaver():
+    def __init__(self, *args, **kwargs):
+        self.title = kwargs.pop('title', 'lang_saver')
+        setproctitle.setproctitle(self.title)
+        self.time = kwargs.pop('time', 0.1)
+        self.logger = logging.getLogger(self.title)
+        handler = SysLogHandler(address='/dev/log')
+        self.logger.setLevel(logging.INFO)
+        self.logger.addHandler(handler)
+        self.logger.info('starting {} with refresh time: {}'.format(self.title, self.time))
+        self.display = kwargs.pop('display', Display())
+        self.xkb = kwargs.pop('xkb', XKeyboard())
+
+    def run(self):
+        window_history = {}
+        prew_window = 0
+        prev_group = self.xkb.group_num
+        while True:
+            window = self.display.get_input_focus().focus.id
+            if window != prew_window:
+                #  window has been changed
+                #  need to check if current group match stored
+                #  and if not - restore from stored
+                if window in window_history and self.xkb.group_num != window_history[window]:
+                    self.xkb.group_num = window_history[window]
+                    self.logger.info('restore lang: {} for window: {}'.format( window_history[window], window))
+                else:
+                    window_history[window] = self.xkb.group_num
+                prew_window = window
+            else:
+                # window wans't changed
+                # probably group changed
+                # check previous group and if not match - update history
+                if prev_group != self.xkb.group_num:
+                    window_history[window] = self.xkb.group_num
+                    prev_group = self.xkb.group_num
+            try:
+                sleep(self.time)
+            except KeyboardInterrupt:
+                sys.exit(0)
+
+if __name__ == '__main__':
+    ls = LangSaver()
+    ls.run()
