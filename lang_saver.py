7f36c7a79b9710c3816b7a5e9dab27685b23735a
Alex Bezpalko <alexbe@wix.com>
2019-10-22 15:52:50 +0300

    rewrite for i3/sway

--- lang_saver.py
+++ lang_saver.py
@@ -4,58 +4,95 @@
 """
 
 import logging
+import os
 from logging.handlers import SysLogHandler
 from time import sleep
 from xkbgroup import XKeyboard
 from Xlib.display import Display
 import setproctitle
 import sys
+import i3ipc
+from systemd.journal import JournaldLogHandler
+
+encodings = ['English (US, intl., with dead keys)', 'Hebrew', 'Ukrainian']
+xkb_layouts = ['us(intl)', 'il', 'ua(unicode)']
+window_layouts = {}
 
 
-class LangSaver():
-    def __init__(self, *args, **kwargs):
-        self.title = kwargs.pop('title', 'lang_saver')
-        setproctitle.setproctitle(self.title)
-        self.time = kwargs.pop('time', 0.1)
-        self.logger = logging.getLogger(self.title)
-        handler = SysLogHandler(address='/dev/log')
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(handler)
-        self.logger.info('starting {} with refresh time: {}'.format(self.title, self.time))
-        self.display = kwargs.pop('display', Display())
-        self.xkb = kwargs.pop('xkb', XKeyboard())
+def get_layout(encoding):
+    global encodings, xkb_layouts
+    if encoding in encodings:
+        return xkb_layouts[encodings.index(encoding)]
+    else:
+        return 'us(intl)'
 
-    def run(self):
-        window_history = {}
-        prew_window = 0
-        prev_group = self.xkb.group_num
-        while True:
-            try:
-                window = self.display.get_input_focus().focus.id
-            except AttributeError:
-                continue
-            if window != prew_window:
-                #  window has been changed
-                #  need to check if current group match stored
-                #  and if not - restore from stored
-                if window in window_history and self.xkb.group_num != window_history[window]:
-                    self.xkb.group_num = window_history[window]
-                    self.logger.info('restore lang: {} for window: {}'.format( window_history[window], window))
-                else:
-                    window_history[window] = self.xkb.group_num
-                prew_window = window
-            else:
-                # window wans't changed
-                # probably group changed
-                # check previous group and if not match - update history
-                if prev_group != self.xkb.group_num:
-                    window_history[window] = self.xkb.group_num
-                    prev_group = self.xkb.group_num
-            try:
-                sleep(self.time)
-            except KeyboardInterrupt:
-                sys.exit(0)
+
+def main():
+    setproctitle.setproctitle(os.path.basename(sys.argv[0]))
+    i3 = i3ipc.Connection()
+    log = logging.getLogger(os.path.basename(sys.argv[0]))
+    journald_handler = JournaldLogHandler()
+    # journald_handler.setFormatter(logging.Formatter(
+    #     '[%(levelname)s] %(message)s'
+    # ))
+    log.addHandler(journald_handler)
+    log.setLevel(logging.DEBUG)
+    pid_file = f'/run/user/{os.getuid()}/{os.path.basename(sys.argv[0])}.pid'
+    if os.path.exists(pid_file):
+        log.critical(f'pid file exists {pid_file}')
+        print(f'pid file exists {pid_file}')
+        sys.exit()
+    f = open(pid_file, 'w')
+    f.write(f'{os.getpid()}')
+    f.close()
+
+    def get_input() -> i3ipc.InputReply:
+        for input_object in i3.get_inputs():
+            if input_object.identifier == '1:1:AT_Translated_Set_2_keyboard':
+                # TODO: get keyboard from cmd arguments
+                return input_object
+
+    def on_input(self: i3ipc.Connection, ev: i3ipc.InputEvent):
+        global window_layouts
+        focused_window = i3.get_tree().find_focused()
+        window_layouts[focused_window.id] = ev.input
+        log.info(
+            f'store window: {focused_window.id} {ev.change} {ev.input.xkb_active_layout_name} {ev.input.xkb_layout_names}')
+
+    def on_focus(self: i3ipc.Connection, ev: i3ipc.WindowEvent):
+        global window_layouts
+        stored_input = None
+        try:
+            stored_input = window_layouts[ev.container.id]
+        except KeyError:
+            log.info(f'no input stored for window {ev.container.id}')
+            current_input = get_input()
+            window_layouts[ev.container.id] = current_input
+            log.info(
+                f'store window: {ev.container.id} xkb_layout {current_input.xkb_active_layout_name} {current_input.xkb_layout_names}')
+            return
+        current_input = get_input()
+        if current_input.xkb_active_layout_index == stored_input.xkb_active_layout_index and current_input.xkb_layout_names == stored_input.xkb_layout_names:
+            log.info(f'window: {ev.container.id}. everything match. no changes required')
+            return
+        else:
+            main_lang = get_layout(stored_input.xkb_active_layout_name)
+            second_lang = get_layout(
+                list(filter(lambda x: x != stored_input.xkb_active_layout_name, stored_input.xkb_layout_names))[0])
+            log.info(
+                f'update: input {stored_input.identifier} xkb_layout "{main_lang},{second_lang}"')
+            result = i3.command(f'input {stored_input.identifier} xkb_layout "{main_lang},{second_lang}"')
+            if result[0].error:
+                log.warning(f'error: {result[0].error}')
+
+    # Subscribe to events
+    i3.on("window::focus", on_focus)
+    i3.on("input::xkb_keymap", on_input)
+    i3.on("input::xkb_layout", on_input)
+    # Start the main loop and wait for events to come in.
+    i3.main()
+    os.unlink(pid_file)
+
 
 if __name__ == '__main__':
-    ls = LangSaver()
-    ls.run()
+    main()
